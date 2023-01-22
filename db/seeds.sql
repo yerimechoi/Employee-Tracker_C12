dfc766b264a0fafc10ad543f7d3249d110190f16
yerimechoi <yerimechoi@gmail.com>
2023-01-21 21:39:07 -0500

    added "s" to role and changed null to 0

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
 INSERT INTO employee (fullName, role_id, manager_id)
-VALUES ("John Doe", "1", "null"),
+VALUES ("John Doe", "1", "0"),
        ("Mike Chan", "2", "1"),
-       ("Ashley Rodriguez", "3", "null"),
+       ("Ashley Rodriguez", "3", "0"),
        ("Kevin Tupik", "4", "3"),
-       ("Kunal Singh", "5", "null")
+       ("Kunal Singh", "5", "0"),
        ("Malia Brown", "6", "5"),
-       ("Sarah Lourd", "7", "null"),
-       ("Tom Allen", "8", "7"),
+       ("Sarah Lourd", "7", "0"),
+       ("Tom Allen", "8", "7");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Lead", "100000", "4"),
        ("Salesperson", "80000", "4"),
        ("Lead Engineer", "100000", "1"),
