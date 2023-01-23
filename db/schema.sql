ac29bdb733af2c9e1c62c8da1af9254fc6c0c016
yerimechoi <yerimechoi@gmail.com>
2023-01-23 18:27:14 -0500

    Added the correct mysql syntax

--- db/schema.sql
+++ db/schema.sql
@@ -11,21 +11,24 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
-    -- FOREIGN KEY (department_id)
-    -- REFERENCES department(id)
-    -- ON DELETE SET NULL
+    department_id INT,
+    CONSTRAINT department
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     fullName VARCHAR(60) NOT NULL,
     role_id INT,
-    -- FOREIGN KEY (role_id)
-    -- REFERENCES roles(id)
-    -- ON DELETE SET NULL,
-    manager_id INT
-    -- FOREIGN KEY (manager_id)
-    -- REFERENCES employee(id)
-    -- ON DELETE SET NULL
+    manager_id INT,
+    CONSTRAINT roles
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE,
+    CONSTRAINT employee
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE CASCADE
 );
